343175b7c33a1243c113eb9b5b909c0a6381eb2f
austin <18511828634@163.com>
2023-06-15 13:24:34 +0800

    fix

--- new.go
+++ new.go
@@ -2,12 +2,19 @@ package gins
 
 import "github.com/gin-gonic/gin"
 
-func NewApp(ms ...func(ctx *gin.Context)) {
-	app := gin.New()
+var app *gin.Engine
+
+func NewApp(ms ...func(ctx *gin.Context)) *gin.Engine {
+	app = gin.New()
 	if len(ms) == 0 {
 		ms = append(ms, gin.Recovery())
 	}
 	for _, m := range ms {
 		app.Use(m)
 	}
+	return app
+}
+
+func GetApp() *gin.Engine {
+	return app
 }
