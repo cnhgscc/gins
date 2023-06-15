b5abf81bd6f9937db631c8451d3a50d50027f3a2
austin <18511828634@163.com>
2023-06-15 11:22:02 +0800

    fix

---
+++ new_server.go
@@ -0,0 +1,39 @@
+package gins
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func Serve(h http.Handler, host string, port int) {
+	addr := fmt.Sprintf("%s:%d", host, port)
+
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: h,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+		log.Println("serve: " + addr)
+
+	}()
+
+	q := make(chan os.Signal, 1)
+	signal.Notify(q, syscall.SIGINT, syscall.SIGTERM)
+	<-q
+	log.Println("shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	_ = srv.Shutdown(ctx)
+	log.Println("server exiting")
+}
