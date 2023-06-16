30941e58758a2d7c0118e255908afcdbf748085b
austin <18511828634@163.com>
2023-06-16 14:21:17 +0800

    fix

--- cors.go
+++ cors.go
@@ -11,7 +11,6 @@ func Cors() func(ctx *gin.Context) {
 		ctx.Header("Access-Control-Allow-Origin", "*")
 		ctx.Header("Access-Control-Allow-Methods", "POST,OPTIONS")
 		ctx.Header("Access-Control-Allow-Headers", "*")
-		//放行所有OPTIONS方法
 		method := ctx.Request.Method
 		if method == "OPTIONS" {
 			ctx.AbortWithStatus(http.StatusNoContent)
