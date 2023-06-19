ffe567c4d8baf31347ea69051b818d6f4c6ce1a0
austin <18511828634@163.com>
2023-06-19 11:17:54 +0800

    fix

---
+++ require.go
@@ -0,0 +1,21 @@
+package gins
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Require(methods ...string) func(ctx *gin.Context) {
+	_require := map[string]bool{}
+	for _, s := range methods {
+		_require[s] = true
+	}
+	return func(ctx *gin.Context) {
+		method := ctx.Request.Method
+		if _, ok := _require[method]; !ok {
+			ctx.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+	}
+}
