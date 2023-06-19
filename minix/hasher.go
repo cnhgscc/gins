057075a2dc9da4947d2182717b72ab81d74441f1
austin <18511828634@163.com>
2023-06-19 13:42:08 +0800

    fix

---
+++ minix/hasher.go
@@ -0,0 +1,17 @@
+package minix
+
+import (
+	"fmt"
+	"hash/crc32"
+	"strings"
+)
+
+func Hasher(tags ...string) (string, string) {
+	field := strings.Join(tags, "_")
+	crc := fmt.Sprintf("%08x", crc32.ChecksumIEEE([]byte(field)))
+	u := crc[0]
+	if !(u >= '0' && u <= '9') {
+		return field, strings.ToLower(crc)[:6]
+	}
+	return field, strings.ToLower(string(u-'0'+'a') + crc[1:6])
+}
